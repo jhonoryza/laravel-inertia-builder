cab04f7e92a8ef9c5e3c745a87f628324188640b
fajar sidik priatna <jardik.oryza@gmail.com>
2025-07-08 12:00:39 +0700

    initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jhonoryza\InertiaBuilder;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    public function register()
+    {
+        
+    }
+    
+    public function boot()
+    {
+        
+    }
+}
